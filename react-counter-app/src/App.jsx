b004037038d84638b7cf00af52a7e98934aaf69d
MdFazilGITHUB <mjam7221jmi@gmail.com>
2023-12-28 12:33:40 +0530

    Commit

---
+++ react-counter-app/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      count: 0
+    }
+  }
+
+  inc = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1
+    }));
+  }
+  dec = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1
+    }));
+  }
+  reset = () => {
+    this.setState({
+      count: 0
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.inc}>Increase</button>
+        <button onClick={this.dec}>Deacrease</button>
+        <button onClick={this.reset}>Reset</button>
+        
+      </div>
+    )
+  }
+}
